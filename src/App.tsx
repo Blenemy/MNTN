49da3906f28e00e3d8cce3b7b39eac97a8034c2e
Denys Topchyi <miliarder97@gmail.com>
2024-05-15 14:55:11 +0300

    ref: refactored components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./styles/_reset.scss";
+import "./App.scss";
+import Parallax from "./components/Parallax/Parallax";
 
 function App() {
-  return <div className="App">123</div>;
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Parallax />
+      </div>
+    </div>
+  );
 }
 
 export default App;
